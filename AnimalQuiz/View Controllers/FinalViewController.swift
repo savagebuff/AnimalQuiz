42aec6fc0048d1d37206d949b9d944ad7d23cb09
choney <garaev-02@list.ru>
2021-11-10 14:02:51 +0300

    add Views

---
+++ AnimalQuiz/View Controllers/FinalViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FinalViewController.swift
+//  AnimalQuiz
+//
+//  Created by Dinar Garaev on 10.11.2021.
+//
+
+import UIKit
+
+class FinalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ AnimalQuiz/View Controllers/QuestionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionViewController.swift
+//  AnimalQuiz
+//
+//  Created by Dinar Garaev on 10.11.2021.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+}
